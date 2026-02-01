5300d8e81dcb4f8e8a7d02f67c8ea0af77c56217
Dioni A. Oliveira <dionialves@gmail.com>
2026-02-01 11:17:53 -0300

    Retirado relacionamento de Client com Order

--- src/main/java/com/dionialves/snapdogdelivery/client/Client.java
+++ src/main/java/com/dionialves/snapdogdelivery/client/Client.java
@@ -5,15 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.List;
-
-import com.dionialves.snapdogdelivery.order.Order;
 
 @Entity
 @Getter
